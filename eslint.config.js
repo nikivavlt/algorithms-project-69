6090ca220fbf18a61e70d4b07039085a96684255
nikivavlt <nikivavlt@gmail.com>
2025-09-30 07:44:45 +0300

    Fix ESLint config issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
 import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
